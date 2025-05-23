ba0b6851d611be04c888628d3ba38149ba89f95b
AshasriSuravarapu99 <155606512+AshasriSuravarapu99@users.noreply.github.com>
2025-05-23 21:28:45 +0530

    Create Creating-Traverse-Count.cpp

---
+++ LinkedList/Creating-Traverse-Count.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+	int data;
+	Node* next;
+public:
+	// Node(int data1,Node* next1)
+	// {
+	//      data = data1;
+	//      next = next1;
+	// }
+
+	Node(int data1)
+	{
+	     data = data1;
+	     next = nullptr;
+	}
+};
+Node* convertArrToList(vector<int> &a)
+{
+    Node* head = new Node(a[0]);
+    Node* mover = head;
+    for(int i=1;i<a.size();i++)
+    {
+    	Node* temp = new Node(a[i]);
+    	mover->next = temp;
+    	mover=temp;
+    }
+    return head;
+}
+int main()
+{
+	vector<int>a = {5,2,3,4,5};
+	Node* head = convertArrToList(a);
+// 	cout<<head->data;
+    Node* temp = head;
+    int length = 0;
+    while(temp)
+    {
+        cout<<temp->data<<" ";
+        length++;
+        temp = temp->next;
+    }
+    cout<<"\nLength "<<length;
+	return 0;
+}
